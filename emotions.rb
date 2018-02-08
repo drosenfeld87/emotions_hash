a6fae6f9ea16a54792e8a4083998ae1f170e5680
debbie <drosenfeld87@gmail.com>
2018-02-07 21:20:14 -0500

    start emotions exercise

---
+++ emotions.rb
@@ -0,0 +1,3 @@
+require './main.rb'
+
+emotions = {:happy => (1..3), :sad => (1..3) :anxious => (1..3)}
---
+++ main.rb
@@ -0,0 +1,8 @@
+class Person
+
+  def initialize(name, emotions)
+    @name = name
+    @emotions = emotions
+  end
+
+  
